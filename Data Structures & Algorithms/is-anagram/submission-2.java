8d468aadb7c0c7ab265e8250650530d364110912
Yue Pan <97856269+bcpy1314@users.noreply.github.com>
2026-08-26 17:11:58 -0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        // build an array of char frequency for both String
+        // compare the array 
+        if (s.length() != t.length()){
+            return false;
+        }
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        for (int val : count){
+            if (val != 0){
+                return false;
+            }
+        }
+        return true;
+ 
+    }
+}
